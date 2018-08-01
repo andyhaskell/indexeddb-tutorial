d04befa2d23a770c4a26fc60fc7eb6bc89c9fe14
Andrew "&y" Haskell <xcrnr2013@gmail.com>
2018-08-01 13:47:37 -0400

    Add functionality for retrieving and displaying sticky notes

--- app/db.js
+++ app/db.js
@@ -20,6 +20,7 @@ dbReq.onupgradeneeded = function(event) {
 dbReq.onsuccess = function(event) {
   // Set the db variable to our database so we can use it!  
   db = event.target.result;
+  getAndDisplayNotes(db);
 }
 
 // Fires when we can't open the database
@@ -43,7 +44,7 @@ function addStickyNote(db, message) {
   store.add(note);
 
   // Wait for the database transaction to complete
-  tx.oncomplete = function() { console.log('stored note!') }
+  tx.oncomplete = function() { getAndDisplayNotes(db); }
   tx.onerror = function(event) {
     alert('error storing note ' + event.target.errorCode);
   }
@@ -65,6 +66,41 @@ function addManyNotes(db, messages) {
   tx.oncomplete = function() { console.log('transaction complete') };
 }
 
+// getAndDisplayNotes retrieves all notes in the notes object store using an
+// IndexedDB cursor and sends them to displayNotes so they can be displayed
+function getAndDisplayNotes(db) {
+  let tx = db.transaction(['notes'], 'readonly');
+  let store = tx.objectStore('notes');
+
+  // Create a cursor request to get all items in the store, which we collect
+  // in the allNotes array
+  let req = store.openCursor();
+  let allNotes = [];
+
+  req.onsuccess = function(event) {
+    // The result of req.onsuccess is an IDBCursor
+    let cursor = event.target.result;
+
+    if (cursor != null) {
+      // If the cursor isn't null, we got an IndexedDB item. Add it to the
+      // note array and have the cursor continue!
+      allNotes.push(cursor.value);
+
+      // cursor.continue has the cursor move to the next item; we keep looping
+      // through the notes because that causes req.onsuccess to fire again
+      cursor.continue();
+    } else {
+      // If we have a null cursor, it means we've gotten all the items in the
+      // store, so display the notes we got
+      displayNotes(allNotes);
+    }
+  }
+
+  req.onerror = function(event) {
+    alert('error in cursor request ' + event.target.errorCode);
+  }
+}
+
 //
 // DOM manipulation
 //
@@ -75,4 +111,17 @@ function submitNote() {
   let message = document.getElementById('newmessage');
   addStickyNote(db, message.value);
   message.value = '';
+}
+
+// displayNotes takes in an array of sticky note objects and displays them
+// as <li> elements.
+function displayNotes(notes) {
+  let listHTML = '<ul>';
+
+  for (let i = 0; i < notes.length; i++) {
+    let note = notes[i];
+    listHTML += '<li>' + note.text + ' ' +
+      new Date(note.timestamp).toString() + '</li>';
+  }
+  document.getElementById('notes').innerHTML = listHTML;
 }
