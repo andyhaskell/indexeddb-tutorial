045dac8f7321728c1cb2cb986a91df581f958b43
Andrew "&y" Haskell <xcrnr2013@gmail.com>
2019-05-14 08:45:21 -0400

    Add webpack to fix the JavaScript build for the web app
    Because db.js contains a module.exports line, db.js can no longer be
    imported directly into an HTML file. Therefore, to fix this, db.js is
    split into db.js for code directly interacting with IndexedDB, and
    page.js for code that manipulates the DOM. We then use webpack to build
    the web app in the 2-testing-tutorial file, now having index.html
    import its JavaScript from the exported main.js file

--- 2-testing-tutorial/db.js
+++ 2-testing-tutorial/db.js
@@ -73,8 +73,6 @@ function addStickyNote(message, callback) {
   }
 }
 
-let reverseOrder = false;
-
 // getNotes retrieves all notes in the notes object store using an IndexedDB
 // cursor and sends them to the calback passed in
 function getNotes(reverseOrder, callback) {
@@ -103,42 +101,6 @@ function getNotes(reverseOrder, callback) {
   req.onerror = function(event) {
     alert('error in cursor request ' + event.target.errorCode);
   }
-}
-
-//
-// DOM manipulation
-//
-
-// submitNote submits the sticky note in the #newmessage text box to the notes
-// object store
-function submitNote() {
-  let message = document.getElementById('newmessage');
-  addStickyNote(message.value, getAndDisplayNotes);
-  message.value = '';
-}
-
-// getAndDisplayNotes is a helper function for retrieving notes and then
-// having them be displayed with displayNotes.
-function getAndDisplayNotes() { getNotes(reverseOrder, displayNotes); }
-
-// displayNotes takes in an array of sticky note objects and displays them as
-// <li> elements.
-function displayNotes(notes) {
-  let listHTML = '<ul>';
-
-  for (let i = 0; i < notes.length; i++) {
-    let note = notes[i];
-    listHTML += '<li>' + note.text + ' ' +
-      new Date(note.timestamp).toString() + '</li>';
-  }
-  document.getElementById('notes').innerHTML = listHTML;
-}
-
-// flipNoteOrder flips the order of the notes we display from forward to
-// reverse, and vice versa, re-displaying the notes in the updated order.
-function flipNoteOrder(notes) {
-  reverseOrder = !reverseOrder;
-  getAndDisplayNotes();
 }
 
 module.exports = {setupDB, addStickyNote, getNotes};
---
+++ 2-testing-tutorial/webpack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  entry: __dirname + "/page.js",
+  output: {
+    path: __dirname,
+  }
+}
